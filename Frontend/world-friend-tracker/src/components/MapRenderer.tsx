fe947f98a88497373d93e99b42ceae51fa6f0c7f
lithial <jameshessell@gmail.com>
2021-04-05 12:33:52 +1200

    feat: map marker loads now.

--- Frontend/world-friend-tracker/src/components/MapRenderer.tsx
+++ Frontend/world-friend-tracker/src/components/MapRenderer.tsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import '../App.css'
-import styles from "./MapRenderer.module.scss"
-import { render } from '@testing-library/react';
+import '../css/Map.css'
+import L from 'leaflet';
+import mapIcon from '../assets/venue_location_icon.svg'
+
 
 type MapProps = {
     currentLocation: { lat: 52.52437, lng: 13.41053 },
     zoom: 12,
 }
+const icon = L.icon({
+  iconUrl: mapIcon,
+  iconSize: [64, 64],
+})
 export default class MapRenderer extends Component<any,MapProps>{
  constructor(props:MapProps){
      super(props);
@@ -21,16 +26,16 @@ export default class MapRenderer extends Component<any,MapProps>{
     render(){  
     const { currentLocation, zoom} = this.state; 
     return(
-        <MapContainer center={currentLocation} zoom={12} scrollWheelZoom={false}>
+        <MapContainer center={currentLocation} zoom={3} scrollWheelZoom={false}>
         <TileLayer subdomains={['mt0','mt1','mt2','mt3']}attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                    url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
         />
-{/*     <Marker position={currentLocation}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker> */}
-</MapContainer>
+        <Marker position={currentLocation} icon={icon}>
+          <Popup>
+            Lithial<br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     )
     }
 }
