e6c6528fb6b3fdd96e52629c4010758d2ca7ff2f
lithial <jameshessell@gmail.com>
2021-04-24 12:56:48 +1200

    feat: basics of friend markers

--- Frontend/atlas/src/components/MapRenderer/MapRenderer.js
+++ Frontend/atlas/src/components/MapRenderer/MapRenderer.js
@@ -5,14 +5,15 @@ import "./MapRenderer.css"
 import mapIcon from "../../assets/venue_location_icon.svg"
 import L from "leaflet";
 import {useUser} from "../../contexts/UserContext";
+import FriendMarkers from "./FriendMarkers";
+import {Map} from "leaflet/dist/leaflet-src.esm";
 
 /*These imports need to be in this order for the map to load properly*/
 
 const MapRenderer = () => {
-    const {showLocation} = useUser();
     const [zoom,setZoom] = useState(3);
     const [centrePosition,setCentrePosition] = useState([5, 176])
-    const {currentCoords} = useUser();
+    const {nickname, timezone,showLocation,currentCoords,friends} = useUser();
     const icon = L.icon({
         iconUrl: mapIcon,
         iconSize: [64, 64],
@@ -24,13 +25,48 @@ const MapRenderer = () => {
 
     return (
         <div>
-            <MapContainer center={centrePosition} zoom={zoom}
-                          scrollWheelZoom={true} >
+            <MapContainer
+                center={centrePosition}
+                zoom={zoom}
+                scrollWheelZoom={true}>
                 <TileLayer
                     subdomains={["mt0", "mt1", "mt2", "mt3"]}
                     attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
                     url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"/>
-                {showLocation ? <Marker
+                 {showLocation ?
+                    <Marker
+                    position={currentCoords}
+                    icon={icon}>
+                    <Popup>
+                        {nickname}
+                        <br/>
+                        {timezone}
+                    </Popup>
+                </Marker>
+                    : <></>
+                }
+                {friends.length !== 0 ?
+                    friends.map((friend, index ) => {
+                        console.log(`Loading friend marker for ${friend.nickname}`)
+                        return (
+                            <Marker
+                                position={[friend.lat, friend.lng]}
+                                icon={icon}
+                                key={index}
+                            >
+                                <Popup>
+                                    {friend.nickname}
+                                    <br/>
+                                    {friend.timezone}
+                                </Popup>
+                            </Marker>)
+                    })
+                    : <></>
+                }
+            </MapContainer>
+
+              {/*  {showLocation ?
+                    <Marker
                     position={currentCoords}
                     icon={icon}>
                     <Popup>
@@ -38,8 +74,15 @@ const MapRenderer = () => {
                         <br/>
                         Easily customizable.
                     </Popup>
-                </Marker> : <></>}
-            </MapContainer>
+                </Marker>
+                    : <></>
+                }*/}
+{/*
+                <FriendMarkers/>
+*/}
+{/*
+            </Map>
+*/}
         </div>
     );
 };
