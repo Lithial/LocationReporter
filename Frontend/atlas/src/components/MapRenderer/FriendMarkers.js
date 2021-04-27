011c493e51a6ff705bf1291b7ebe4b741124bfa1
Joe van der Zwet <me@joezwet.me>
2021-04-27 19:37:45 +1200

    feat: update map tiles, add friend times, cluster friends on map

--- Frontend/atlas/src/components/MapRenderer/FriendMarkers.js
+++ Frontend/atlas/src/components/MapRenderer/FriendMarkers.js
@@ -1,31 +1,35 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Marker, Popup} from "react-leaflet";
 import L from "leaflet";
 import {Avatar} from "@material-ui/core";
-import mapIcon from "../../assets/venue_location_icon.svg"
+import * as ReactDOMServer from "react-dom/server";
 
 const FriendMarker = (props) => {
 
-
     const icon = L.divIcon({
-        html:
-        `<img src='${props.friend.picture}' style="border-radius: 50%; height: 48px; width: 48px; margin: 0 0;justify-content: center; align-items: center; padding: 0 0;"/>`
-        ,
-       /* iconUrl: props.friend.picture ? props.friend.picture : mapIcon,*/
-        iconSize: [50, 50],
+        html: ReactDOMServer.renderToString(<Avatar src={props.friend.picture} alt={props.friend.nickname} />),
+        className: "not-a-real-class-to-disable-background"
     });
+
+    const options = {
+            timeZone: props.friend.timeZone,
+            hour: "numeric",
+            minute: "numeric",
+        }
+    const formatter = new Intl.DateTimeFormat([], options);
+
     return (
         <Marker
             position={[props.friend.lat, props.friend.lng]}
             icon={icon}
         >
             <Popup>
-                <Avatar src={`${props.friend.picture}`} />
                 {props.friend.nickname}
                 <br/>
-                {props.friend.timezone}
+                {formatter.format(new Date())} ({props.friend.timezone})
             </Popup>
-        </Marker>)
+        </Marker>
+    )
 };
 
 export default FriendMarker;
--- Frontend/atlas/src/components/MapRenderer/MapRenderer.js
+++ Frontend/atlas/src/components/MapRenderer/MapRenderer.js
@@ -1,38 +1,81 @@
 import React, {useEffect, useState} from "react";
-import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+import {MapContainer, TileLayer} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import 'react-leaflet-markercluster/dist/styles.min.css';
 import "./MapRenderer.css"
-import mapIcon from "../../assets/venue_location_icon.svg"
 import L from "leaflet";
 import {useUser} from "../../contexts/UserContext";
 import FriendMarker from "./FriendMarkers";
 import MarkerClusterGroup from "react-leaflet-markercluster/src/react-leaflet-markercluster";
+import {AvatarGroup} from "@material-ui/lab";
+import * as ReactDOMServer from "react-dom/server";
+import {Avatar} from "@material-ui/core";
 
 /*These imports need to be in this order for the map to load properly*/
+
+const MapIcons = (props) => {
+
+    return (
+        <MarkerClusterGroup iconCreateFunction={
+            (cluster) => {
+                const markers = cluster.getAllChildMarkers() ?? [];
+                const friends = [];
+
+                markers.forEach((value, index) => {
+                    const html = value.options.icon.options.html
+
+                    let name = html.split("alt=\"")[1];
+                    name = name.substring(0, name.indexOf("\""));
+
+                    let img = html.split("src=\"")[1];
+                    img = img.substring(0, img.indexOf("\""));
+
+                    friends.push({name: name, img: img})
+                })
+
+                return L.divIcon({
+                    html: ReactDOMServer.renderToString(
+                        <AvatarGroup max={2} spacing={"small"}>
+                            {
+                                friends.map((value, index) =>
+                                    <Avatar
+                                        key={`${value.name}-${index}`}
+                                        alt={value.name}
+                                        src={value.img} />)
+                            }
+                        </AvatarGroup>
+                    ),
+                    className: "not-a-real-class-to-disable-background"
+                })
+            }
+        } >
+            {
+                props.icons.map((friend, index) => {
+                    return <FriendMarker key={index} friend={friend}/>
+                })
+            }
+        </MarkerClusterGroup>
+    )
+}
 
 const MapRenderer = () => {
     const [zoom, setZoom] = useState(3);
     const [centrePosition, setCentrePosition] = useState([5, 176])
     const {nickname, picture, timezone, showLocation, currentCoords, friends} = useUser();
-
-    const iconList = [...friends, {
-        nickname: nickname,
-        picture: picture,
-        currentCoords: currentCoords,
-        timezone: timezone,
-        lat: currentCoords[0],
-        lng: currentCoords[1],
-        showLocation: showLocation,
-    }]
-    const icon = L.icon({
-        iconUrl: !picture ? picture : mapIcon,
-        iconSize: [64, 64],
-    });
-
+    
+    const [iconList, setIconList] = useState([]);
+    
     useEffect(() => {
-        console.log(currentCoords)
-    }, [currentCoords]);
+        setIconList([...friends, {
+            nickname: nickname,
+            picture: picture,
+            currentCoords: currentCoords,
+            timezone: timezone,
+            lat: currentCoords[0],
+            lng: currentCoords[1],
+            showLocation: showLocation,
+        }])
+    }, [friends, nickname, picture, currentCoords, timezone, showLocation])
 
     return <div>
         <MapContainer
@@ -40,16 +83,12 @@ const MapRenderer = () => {
             zoom={zoom}
             scrollWheelZoom={true}>
             <TileLayer
-                subdomains={["mt0", "mt1", "mt2", "mt3"]}
-                attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
-                url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"/>
-            <MarkerClusterGroup>
-                {
-                    iconList.map((friend, index) => {
-                        return <FriendMarker key={index} friend={friend}/>
-                    })
-                }
-            </MarkerClusterGroup>
+                maxZoom={20}
+                attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"/>
+            {
+                iconList !== [] && <MapIcons icons={iconList} />
+            }
         </MapContainer>
     </div>;
 };
