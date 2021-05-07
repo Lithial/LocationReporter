92bb3bce35700c9f9ff56a68b18c8ff330af1fe0
lithial <jameshessell@gmail.com>
2021-05-07 20:35:51 +1200

    feat: Added the forgotten card

---
+++ Frontend/atlas/src/components/Profile/ProfileLocationCards.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ProfileTextElement from "./ProfileTextElement";
+import {useUser} from "../../contexts/UserContext";
+import {LinearProgress} from "@material-ui/core";
+
+const ProfileLocationCards = () => {
+    const [userData, dispatch] = useUser();
+    if(!userData.locationLoaded){
+        return (
+            <div>
+                <LinearProgress/>
+            </div>
+        )
+    }
+    if(userData.user.country === "" || userData.user.country === undefined || userData.user.country === "undefined"){
+        return(
+            <div>
+
+        </div>
+        )
+    }
+    return (
+        <div>
+            <ProfileTextElement value={userData.user.currentCoords} text={"Current Location: "}/>
+            <ProfileTextElement value={userData.user.country} text={"Country: "}/>
+            <ProfileTextElement value={userData.user.timezone} text={"TimeZone: "}/>
+        </div>
+    );
+};
+
+export default ProfileLocationCards;
